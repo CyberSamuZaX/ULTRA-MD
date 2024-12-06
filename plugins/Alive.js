88a46b8e8774059297e33c5a5b84c573ee70236a
Mr.Pasindu <140577992+CyberSamuZaX@users.noreply.github.com>
2024-12-06 17:13:21 +0530

    Update Alive.js

--- plugins/Alive.js
+++ plugins/Alive.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   // Sound
   let name = m.pushName || conn.getName(m.sender);
-  let img = 'https://i.imgur.com/s0SqG3g.jpeg';
+  let img = 'https://avatars.githubusercontent.com/u/140577992?v=4.jpg';
   let con = {
     key: {
       fromMe: false,
@@ -17,14 +17,14 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   };
 
   let messageContent = {
-    text: '𝗨𝗟𝗧𝗥𝗔-𝗠𝗗 𝗜𝗦 𝗥𝗨𝗡𝗡𝗜𝗡𝗚', // Text content in case a message body is needed
+    text: 'DARK-SAMUZA IS RUNNING', // Text content in case a message body is needed
     contextInfo: {
       mentionedJid: [m.sender],
       externalAdReply: {
-        title: '© 𝙶𝚕𝚘𝚋𝚊𝚕𝚃𝚎𝚌𝚑𝙸𝚗𝚏𝚘',
-        body: '© GlobalTechInfo',
+        title: '© CYBER-SAMUZA',
+        body: '© CYBER-SAMUZA',
         thumbnailUrl: img,
-        sourceUrl: 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07',
+        sourceUrl: 'wa.me/+94765718443',
         mediaType: 1,
         renderLargerThumbnail: true,
       },
